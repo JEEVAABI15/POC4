4d30b9395e275c32292fd40c4aeeb5da7707ca21
JEEVA ABISHAKE <jeevaabishake2002@gmail.com>
2024-09-27 21:58:46 +0530

    Delete configure_nodejs.groovy

--- configure_nodejs.groovy
+++
@@ -1,9 +0,0 @@
-import jenkins.model.*
-import hudson.tools.*
-
-def instance = Jenkins.getInstance()
-def nodeJSInstaller = new NodeJSInstaller("node", null)
-
-def nodeJSInstallation = new NodeJSInstallation("node", [nodeJSInstaller], null)
-instance.getDescriptorByType(NodeJSInstallation.DescriptorImpl.class).setInstallations(nodeJSInstallation)
-instance.save()
